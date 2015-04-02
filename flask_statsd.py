90c8f6b885d105ec546334f89a60b8b4f1222280
Robert McQueen <rob@systemizer.me>
2015-04-02 11:25:10 -0700

    Adding documentation. adding statsd client with metric naming. updating tests

--- flask_statsd.py
+++ flask_statsd.py
@@ -6,72 +6,125 @@ from flask import request, current_app
 import statsd
 
 class Statsd(object):
-    '''Statsd Extension for Flask'''
-    START_TIME_ATTR = "statsd_start_time"
+    '''Statsd Extension for Flask
 
-    _client = None
+    This extension does a couple things
+    1. Creates status code and latency metrics for all app request handlers
+    2. Provides a client with flask-specific metric naming so you don't have
+       to do it inline.
+
+    - - - - - - - - - - - -
+
+    app = Flask('myapp')
+    statsd = Statsd(app, config={'STATSD_PREFIX': 'myprefix'})
+
+    @app.route('/')
+    def index():
+
+        statsd.incr('doing_stuff')
+        print 'DOING STUFF'
+
+        with statsd.timer('doing_more_stuff'):
+          print 'DOING MORE STUFF'
+
+        return 'OK'
+
+    - - - - - - - - - - - -
+
+    Metrics follow the naming scheme:
+    <prefix (optional)>.<app_name>.<metric_name>
+
+    The following code will produce the following metrics:
+     - COUNTER: stats.myprefix.myapp.doing_stuff
+     - COUNTER: stats.myprefix.myapp.request_handlers.index.<status_code>
+     - TIMER: stats.timers.myprefix.myapp.request_handlers.index.<status_code>
+     - TIMER: stats.timers.myprefix.myapp.doing_more_stuff
+
+
+    '''
+    START_TIME_ATTR = "statsd_start_time"
+    __client = None
 
     def __init__(self, app=None):
-        self.app = app
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
-        app.config.setdefault('STATSD_ENABLED', True)
+        app.config.setdefault('STATSD_ENABLE_REQUEST_HANDLERS', True)
         app.config.setdefault('STATSD_HOST', 'localhost')
         app.config.setdefault('STATSD_PORT', 8125)
         app.config.setdefault('STATSD_PREFIX', None)
         app.config.setdefault('STATSD_RATE', 1)
 
-        if app.config['STATSD_ENABLED']:
+        if app.config['STATSD_ENABLE_REQUEST_HANDLERS']:
             app.before_request(self._before_request)
             app.after_request(self._after_request)
 
+    ###
+    # Main Interface (proxy commands to pystatsd)
+    ###
+
+
+    def timer(self, stat, rate=None):
+        return self._client.timer(
+            rate = rate or current_app.config['STATSD_RATE'],
+            stat = self._metric_name(stat)
+        )
+
+    def timing(self, stat, delta, rate=None):
+        return self._client.timing(
+            rate = rate or current_app.config['STATSD_RATE'],
+            delta = delta,
+            stat = self._metric_name(stat)
+        )
+
+    def incr(self, stat, count=1, rate=None):
+        return self._client.incr(
+            rate = rate or current_app.config['STATSD_RATE'],
+            count = count,
+            stat = self._metric_name(stat)
+        )
+
+    def decr(self, stat, count=1, rate=None):
+        return self.incr(stat, -count, rate)
+
+    ###
+    # HELPERS
+    ###
+
+    def _metric_name(self, name):
+        return "%s.%s" % (current_app.name, name)
+
+    @property
+    def _client(self):
+        if not self.__client:
+            self.__client = statsd.StatsClient(
+                current_app.config['STATSD_HOST'],
+                current_app.config['STATSD_PORT'],
+                current_app.config['STATSD_PREFIX']
+            )
+        return self.__client
+
+
+    ###
+    # REQUEST HANDLER FUNCTIONALITY
+    ###
     def _after_request(self, response):
         if not hasattr(request, self.START_TIME_ATTR) or \
            current_app.config['STATSD_RATE'] == 0:
             return
 
-        metric_name = self._metric_name(suffix = response.status_code)
+        metric_name = ".".join(["request_handlers",
+                                request.endpoint,
+                                str(response.status_code)])
 
-        self.client.timing(
-            stat = metric_name,
-            delta = time.time() - getattr(request, self.START_TIME_ATTR),
-            rate = current_app.config['STATSD_RATE']
+        self.timing(
+            metric_name,
+            int((time.time() - getattr(request, self.START_TIME_ATTR))*1000)
         )
-
-        self.client.incr(
-            stat = metric_name,
-            count = int(1 / current_app.config['STATSD_RATE']),
-            rate = current_app.config['STATSD_RATE']
-        )
+        self.incr(metric_name)
 
         return response
 
     def _before_request(self):
         setattr(request, self.START_TIME_ATTR, time.time())
-
-    def _metric_name(self, prefix="request_handlers", suffix=None):
-        '''
-        This converts the request paths into valid statsd metric names.
-        It unquotes the url, removes trailing slash, and converts
-        slashes and whitespace to dashes.
-        '''
-        metric_name = request.endpoint
-
-        if prefix:
-            metric_name = "%s.%s" % (prefix, metric_name)
-        if suffix:
-            metric_name = "%s.%s" % (metric_name, suffix)
-
-        return metric_name
-
-    @property
-    def client(self):
-        if not self._client:
-            self._client = statsd.StatsClient(
-                current_app.config['STATSD_HOST'],
-                current_app.config['STATSD_PORT'],
-                current_app.config['STATSD_PREFIX']
-            )
-        return self._client
--- test.py
+++ test.py
@@ -6,45 +6,53 @@ import flask_statsd
 
 class FlaskStatsdTestCase(unittest.TestCase):
     def setUp(self):
-        app = flask.Flask(__name__)
+        app = flask.Flask("myapp")
+        statsd = flask_statsd.Statsd(app)
+        self.test_client = app.test_client()
 
         @app.route('/')
         def index():
+            statsd.incr("test_incr")
+
+            with statsd.timer("test_timer"):
+                pass
+
             return "Test", 200
 
         @app.route('/fail')
         def fail():
             return "FAIL", 500
 
-        self.statsd = flask_statsd.Statsd(app)
-
-        self.test_client = app.test_client()
 
     def test_successful_response(self):
-        with mock.patch('flask_statsd.Statsd.client') as mock_client:
+        with mock.patch('flask_statsd.Statsd._client') as mock_client:
             self.test_client.get("/")
 
-            mock_client.incr.assert_called_with(
-                stat = 'request_handlers.index.200',
-                count = mock.ANY,
-                rate = mock.ANY)
+            mock_client.incr.call_args_list == [
+                mock.call(count=1, stat='myapp.test_incr', rate=1),
+                mock.call(count=1, stat='myapp.request_handlers.index.200', rate=1)
+            ]
 
             mock_client.timing.assert_called_with(
-                stat = 'request_handlers.index.200',
+                stat = 'myapp.request_handlers.index.200',
                 delta = mock.ANY,
                 rate = mock.ANY)
 
+            mock_client.timer.assert_called_with(
+                stat = 'myapp.test_timer',
+                rate = mock.ANY)
+
     def test_failed_response(self):
-        with mock.patch('flask_statsd.Statsd.client') as mock_client:
+        with mock.patch('flask_statsd.Statsd._client') as mock_client:
             self.test_client.get("/fail")
 
             mock_client.incr.assert_called_with(
-                stat = 'request_handlers.fail.500',
-                count = mock.ANY,
+                stat = 'myapp.request_handlers.fail.500',
+                count = 1,
                 rate = mock.ANY)
 
             mock_client.timing.assert_called_with(
-                stat = 'request_handlers.fail.500',
+                stat = 'myapp.request_handlers.fail.500',
                 delta = mock.ANY,
                 rate = mock.ANY)
 
